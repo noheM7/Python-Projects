7bc8619ef3e179fd92b77fceb6c423713114456e
Nohe <nmesfien@gmail.com>
2023-03-01 14:55:01 -0800

    Update file_transfer.py

--- file transfer/file_transfer.py
+++ file transfer/file_transfer.py
@@ -1,6 +1,8 @@
 import tkinter as tk
 from tkinter import *
-import tkinter.filedialog 
+import tkinter.filedialog
+import os
+import shutil
 
 
 class ParentWindow(Frame):
@@ -18,15 +20,29 @@ class ParentWindow(Frame):
             self.destDir_btn.grid(row=1, column=0, padx=(20,10), pady=(15,10))
             self.destination_dir= Entry(width=75)
             self.destination_dir.grid(row=1, column=1, columnspan=2, padx=(20,10), pady=(15, 10))
+
     def sourceDir(self):
             selectSourceDir= tkinter.filedialog.askdirectory()
             self.source_dir.delete(0,END)
             self.source_dir.insert(0,selectSourceDir)
+            
     def destDir(self):
             selectDestDir=tkinter.filedialog.askdirectory()
             self.destination_dir.delete(0,END)
             self.destination_dir.insert(0, selectDestDir)
 
+    def transferFiles(self):
+            source=self.source_dir.get()
+            destination=self.destination_dir.get()
+            source_files=os.listdir(source)
+            for i in source_files:
+                shutil.move(source + '/' + i, destination)
+                print(i +'was successfully transferred.')
+                self.transfer_btn= Button(text="Transfer Files", width=20, command=self.transferFiles)
+                self.transfer_btn.grid(row=2, column=1, padx=(200,0), pady=(0,15))
+
+
+               
 
 if __name__== "__main__":
     root=tk.Tk()
