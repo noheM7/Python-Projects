c37ad72fcd6b3265397ec0b8e2ab1ff1dd6d8eee
Nohe <nmesfien@gmail.com>
2023-03-02 04:29:01 -0800

    Update file_transfer.py

--- file transfer/file_transfer.py
+++ file transfer/file_transfer.py
@@ -1,3 +1,15 @@
+# author: Nohe Mesfien
+
+#date:03/2/23
+
+#python 3.10
+
+#objective: Transfer files through directory from one place to another.
+
+
+
+
+
 import tkinter as tk
 from tkinter import *
 import tkinter.filedialog
@@ -8,7 +20,7 @@ import shutil
 class ParentWindow(Frame):
     def __init__(self, master):
          
-            
+            #buttons used for functions
             Frame.__init__(self)
             self.master.title("File Transfer")
             self.sourceDir_btn=Button(text="Select source", width=20, command=self.sourceDir)
@@ -20,17 +32,28 @@ class ParentWindow(Frame):
             self.destDir_btn.grid(row=1, column=0, padx=(20,10), pady=(15,10))
             self.destination_dir= Entry(width=75)
             self.destination_dir.grid(row=1, column=1, columnspan=2, padx=(20,10), pady=(15, 10))
+            
+            self.transfer_btn=Button(text="Transfer Files", width=20, command=self.transferFiles)
+            self.transfer_btn.grid(row=2, column=1, padx=(200,0), pady=(0,15))
 
+            self.exit_btn=Button(text="Exit", width=20, command=self.exit_program)
+            self.exit_btn.grid(row=2, column=2, padx=(10,40), pady=(0,15))
+
+
+        #start of transfer files
     def sourceDir(self):
             selectSourceDir= tkinter.filedialog.askdirectory()
             self.source_dir.delete(0,END)
             self.source_dir.insert(0,selectSourceDir)
             
+        #directs to destination
     def destDir(self):
             selectDestDir=tkinter.filedialog.askdirectory()
             self.destination_dir.delete(0,END)
             self.destination_dir.insert(0, selectDestDir)
 
+
+        #transfer files function
     def transferFiles(self):
             source=self.source_dir.get()
             destination=self.destination_dir.get()
@@ -38,12 +61,13 @@ class ParentWindow(Frame):
             for i in source_files:
                 shutil.move(source + '/' + i, destination)
                 print(i +'was successfully transferred.')
-                self.transfer_btn= Button(text="Transfer Files", width=20, command=self.transferFiles)
-                self.transfer_btn.grid(row=2, column=1, padx=(200,0), pady=(0,15))
+                
+    def exit_program(self):
+        
+        root.destroy()
 
-
-               
-
+#Execute the program
+    
 if __name__== "__main__":
     root=tk.Tk()
     App= ParentWindow(root)
